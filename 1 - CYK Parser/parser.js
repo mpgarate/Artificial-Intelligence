883b95da1ee56a49abf0e769f7c0d713818758dd
Michael Garate <mpgarate@gmail.com>
2014-01-31 17:29:37 -0500

    refactored and began initialize loop

--- 1 - CYK Parser/parser.js
+++ 1 - CYK Parser/parser.js
@@ -1,33 +1,29 @@
-// Get command line args
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
-});
-
 // Get the grammar and lexicon rules for our parser in JSON format
-var RuleSet = function(path){
-  this.contents = require(path);
+var Grammar = function(path){
+  this.rules = require(path).rules;
+  this.ruleCount = this.rules.length;
 }
-
-var grammar = new RuleSet('./grammar.json');
-var lexicon = new RuleSet('./lexicon.json');
+var Lexicon = function(path){
+  this.rules = require(path).rules;
+  this.ruleCount = this.rules.length;
+}
+var grammar = new Grammar('./grammar.json');
+var lexicon = new Lexicon('./lexicon.json');
 
 
 // Get input sentences in JSON format
-var SentenceSet = function(path){
-  this.contents = require(path);
-}
-var sentences = new SentenceSet('./sentences.json');
+var sentences = require('./sentences.json');
 
 var Sentence = function(sentence){
-  this.contents = sentence;
-  this.words = this.contents.split(" "); //array of words 
+  this.string = sentence;
+  this.words = this.string.split(" "); //array of words 
   this.wordCount = this.words.length;
 }
 
-var sentence = new Sentence(sentences.contents[0]); //temporary hard code
+var sentence = new Sentence(sentences[0]); //temporary hard code
 
 // Outline our Tree Node Object
-var Tree = function(){
+var TreeNode = function(){
   this.phrase;         // The non-terminal
   this.startPhrase;    // index of starting word
   this.endPhrase;      // index of ending word
@@ -37,4 +33,23 @@ var Tree = function(){
   this.prob;           // probability
 }
 
-console.log(sentence.wordCount);
+var parse = function(sentence){
+  // Initialize the array
+  var P = [];
+  var N = sentence.wordCount;
+  for(var i = 0; i < N + 1; i++){
+    word = sentence[i];
+    for (var t = 0; t < lexicon.ruleCount; t++){
+      var POS = lexicon.rules[t].pos;
+      var word = lexicon.rules[t].word;
+      P[POS][i][i] = new TreeNode(POS,i,i);
+      console.log(POS);
+    }
+  }
+}
+
+// loop through all of our sentences and parse each
+for (var i = 0; i < 1; i++){
+  var sentence = new Sentence(sentences[i]);
+  parse(sentence);
+}
