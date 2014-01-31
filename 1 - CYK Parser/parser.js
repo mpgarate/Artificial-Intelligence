0cf63304eea77847190b2e42b2955646ae958723
Michael Garate <mpgarate@gmail.com>
2014-01-31 14:24:58 -0500

    added parser

---
+++ 1 - CYK Parser/parser.js
@@ -0,0 +1,26 @@
+/***** Initialization *****/
+
+// Get command line args
+process.argv.forEach(function (val, index, array) {
+	console.log(index + ': ' + val);
+});
+
+
+/***** Outline our Tree Node Object *****/
+
+var Tree = function(){
+	this.phrase;				//The non-terminal
+	this.startPhrase;		// index of starting word
+	this.endPhrase;			// index of ending word
+	this.word;					//If a leaf, then the word
+	this.right;					
+	this.left;					
+	this.prob; 					//probability
+}
+
+myNode = new Tree;
+console.log(myNode.prob);
+
+
+
+/***** Supporting methods *****/
