5add3793261fcbf53dfc0d302341414b7f5def9e
Michael Garate <mpgarate@gmail.com>
2014-01-31 15:16:44 -0500

    created grammar and lexicon json objects

--- 1 - CYK Parser/parser.js
+++ 1 - CYK Parser/parser.js
@@ -3,11 +3,17 @@ process.argv.forEach(function (val, index, array) {
   console.log(index + ': ' + val);
 });
 
+var RuleSet = function(grammar_path, lexicon_path){
+  this.grammar = require(grammar_path);
+  this.lexicon = require(lexicon_path);
+}
+rules = new RuleSet('./grammar.json', './lexicon.json');
+
 
 // Outline our Tree Node Object
 
 var Tree = function(){
-  this.phrase;         //The non-terminal
+  this.phrase;         // The non-terminal
   this.startPhrase;    // index of starting word
   this.endPhrase;      // index of ending word
   this.word;           // If a leaf, then the word
@@ -18,3 +24,11 @@ var Tree = function(){
 
 
 // Supporting methods
+
+/*
+var i;
+for(i = 0; i< 1; i++){
+  console.log(grammar.S[i]);
+}
+*/
+console.log(rules.lexicon);
