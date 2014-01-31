07dc69fe87c052d0ad38ad63d97d76fb121d29bd
Michael Garate <mpgarate@gmail.com>
2014-01-31 14:33:06 -0500

    reformatted comments and cleaned up code

--- 1 - CYK Parser/parser.js
+++ 1 - CYK Parser/parser.js
@@ -1,26 +1,20 @@
-/***** Initialization *****/
-
 // Get command line args
 process.argv.forEach(function (val, index, array) {
 	console.log(index + ': ' + val);
 });
 
 
-/***** Outline our Tree Node Object *****/
+// Outline our Tree Node Object
 
 var Tree = function(){
 	this.phrase;				//The non-terminal
 	this.startPhrase;		// index of starting word
 	this.endPhrase;			// index of ending word
-	this.word;					//If a leaf, then the word
+	this.word;					// If a leaf, then the word
 	this.right;					
 	this.left;					
-	this.prob; 					//probability
+	this.prob; 					// probability
 }
 
-myNode = new Tree;
-console.log(myNode.prob);
 
-
-
-/***** Supporting methods *****/
+// Supporting methods
