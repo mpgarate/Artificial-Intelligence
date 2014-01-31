9ac03df343faef48789d659547289cbc321249f5
Michael Garate <mpgarate@gmail.com>
2014-01-31 14:35:43 -0500

    replace tabs with spaces

--- 1 - CYK Parser/parser.js
+++ 1 - CYK Parser/parser.js
@@ -1,19 +1,19 @@
 // Get command line args
 process.argv.forEach(function (val, index, array) {
-	console.log(index + ': ' + val);
+  console.log(index + ': ' + val);
 });
 
 
 // Outline our Tree Node Object
 
 var Tree = function(){
-	this.phrase;				//The non-terminal
-	this.startPhrase;		// index of starting word
-	this.endPhrase;			// index of ending word
-	this.word;					// If a leaf, then the word
-	this.right;					
-	this.left;					
-	this.prob; 					// probability
+  this.phrase;         //The non-terminal
+  this.startPhrase;    // index of starting word
+  this.endPhrase;      // index of ending word
+  this.word;           // If a leaf, then the word
+  this.right;
+  this.left;
+  this.prob;           // probability
 }
 
 
