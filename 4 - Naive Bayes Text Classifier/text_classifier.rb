c60be0d06910491a22e38629465d3c916bcb31bb
Michael Garate <mpgarate@gmail.com>
2014-04-09 11:38:59 -0400

    refactor file processing

--- 4 - Naive Bayes Text Classifier/text_classifier.rb
+++ 4 - Naive Bayes Text Classifier/text_classifier.rb
@@ -2,68 +2,18 @@ require 'set'
 
 require_relative 'text_classifier/biography.rb'
 require_relative 'text_classifier/word_set.rb'
+require_relative 'text_classifier/input_file_parser.rb'
 
 class TextClassifier
 
   def initialize(path,n_entries)
-    input_file = File.open(path, "r")
-    stopwords_file = File.open('stopwords.txt', "r")
-
-    parse_stopwords_to_set(stopwords_file)
-
-    parse_file_to_bios(input_file)
-
-  end
-
-  private
-
-  def parse_stopwords_to_set(stopwords_file)
-    @stopwords = Set.new
-    stopwords_file.each_line do |l|
-      l.split.each do |word|
-        @stopwords.add(word)
-      end
+    ifp = InputFileParser.new(path)
+    
+    bio = ifp.get_next_bio
+    while bio != nil
+      puts bio
+      
+      bio = ifp.get_next_bio
     end
   end
-
-  def parse_file_to_bios(input_file)
-    @bios = []
-    name_pos = 0
-    category_pos = 1
-    word_set_pos = 2
-
-    name = nil
-    category = nil
-    word_set = Set.new
-
-    current_position = 0
-
-    input_file.each_line do |l|
-      # if empty line
-      if l.split.length == 0 then
-        bio = Biography.new(name, category, word_set)
-        @bios << (bio)
-
-        puts bio
-
-        current_position = 0
-        name = nil
-        category = nil
-        word_set = Set.new
-      elsif current_position == name_pos
-        words = l.split # remove trailing white space
-        name = "#{words[0]} #{words[1]}"
-        current_position += 1
-      elsif current_position == category_pos
-        category = l.split.first # remove trailing white space
-        current_position += 1
-      elsif current_position == word_set_pos
-        l.split.each do |word|
-          word = word.delete "," "."
-          word_set.add(word) unless @stopwords.include? word
-        end
-      end
-    end
-  end
-
 end
---
+++ 4 - Naive Bayes Text Classifier/text_classifier/input_file_parser.rb
@@ -0,0 +1,51 @@
+require 'set'
+class InputFileParser
+  def initialize(path)
+    @input_file = File.open(path, "r")
+
+    stopwords_file = File.open('stopwords.txt', "r")
+    parse_stopwords_to_set(stopwords_file)
+  end
+
+  def parse_stopwords_to_set(stopwords_file)
+    @stopwords = Set.new
+    stopwords_file.each_line do |l|
+      l.split.each do |word|
+        @stopwords.add(word)
+      end
+    end
+  end
+
+  def get_next_bio
+    name_pos = 0
+    category_pos = 1
+    word_set_pos = 2
+
+    name = nil
+    category = nil
+    word_set = Set.new
+
+    current_position = 0
+
+    @input_file.each_line do |l|
+      # if empty line
+      if l.split.length == 0 then
+        bio = Biography.new(name, category, word_set)
+        return bio
+      elsif current_position == name_pos
+        words = l.split # remove trailing white space
+        name = "#{words[0]} #{words[1]}"
+        current_position += 1
+      elsif current_position == category_pos
+        category = l.split.first # remove trailing white space
+        current_position += 1
+      elsif current_position == word_set_pos
+        l.split.each do |word|
+          word = word.delete "," "."
+          word_set.add(word) unless @stopwords.include? word
+        end
+      end
+    end
+      return nil
+  end
+end
