f5729239529573e039a3d30d7e610eac137ee8a4
Michael Garate <mpgarate@gmail.com>
2014-04-19 02:58:16 -0400

    restored probs looking somewhat ok

--- 4 - Naive Bayes Text Classifier/text_classifier/classification.rb
+++ 4 - Naive Bayes Text Classifier/text_classifier/classification.rb
@@ -1,7 +1,9 @@
 class Classification
   def initialize
     @best_match = []
+    @min_match = []
     @matches = Hash.new
+    @value_sum = 0
   end
 
   def best_category
@@ -10,13 +12,32 @@ class Classification
 
   def add(cat, val)
     @matches[cat] = val
+    @value_sum += val
 
     if @best_match.empty? or @best_match[1] > val then
       @best_match = [cat,val]
     end
+
+    if @min_match.empty? or @min_match[1] < val then
+      @min_match = [cat,val]
+    end
   end
 
   def print_detailed_and_compare_to(bio)
+
+    recovered_sum = 0
+    recovered_matches = Hash.new
+    @matches.each do |k,v|
+      rp = recover_probability(v)
+      recovered_matches[k] = rp
+      recovered_sum += rp
+    end
+
+    recovered_matches.each_key do |k|
+      val = recovered_matches[k]
+      recovered_matches[k] = (val / recovered_sum)
+    end
+
     if @best_match[0] == bio.category then
         right_or_wrong = "Right"
       else
@@ -25,15 +46,22 @@ class Classification
 
       puts "#{bio.name}. Prediction: #{@best_match[0]}. #{right_or_wrong}."
 
-      value_sum = 0
       @matches.each_key do |cat|
         print("#{cat}: ")
-        printf("%.2f",@matches[cat])
+        printf("%.2f", recovered_matches[cat])
         print("    ")
 
       end
 
       puts
       puts
+  end
+
+  def recover_probability(val)
+    if val - @min_match[1] < 7 then
+      return 2 ** (@min_match[1] - val)
+    else
+      return 0
+    end
   end
 end
